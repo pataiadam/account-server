fbc59152049955ce4e6bafe3ee26c7d012850229
pataiadam <pataiadam@gmail.com>
2016-01-03 20:53:33 +0100

    Refactor models and views

--- api/models/index.js
+++ api/models/index.js
@@ -1,23 +1,19 @@
+'use strict';
 var Waterline = require('waterline');
 
 module.exports = function(orm){
-    //TODO khmm..
-    var Passport = Waterline.Collection.extend(require('./Passport'));
-    orm.loadCollection(Passport);
-    console.log('Passport loaded')
-    var User = Waterline.Collection.extend(require('./User'));
-    orm.loadCollection(User);
-    console.log('User loaded')
-    var Provider = Waterline.Collection.extend(require('./Provider'));
-    orm.loadCollection(Provider);
-    console.log('Provider loaded')
-    var AuthLog = Waterline.Collection.extend(require('./AuthLog'));
-    orm.loadCollection(AuthLog);
-    console.log('AuthLog loaded')
-    var App = Waterline.Collection.extend(require('./App'));
-    orm.loadCollection(App);
-    console.log('App loaded')
-    var Admin = Waterline.Collection.extend(require('./Admin'));
-    orm.loadCollection(Admin);
-    console.log('Admin loaded')
+    //TODO hmmm..
+    var models = [
+        'Passport',
+        'User',
+        'Provider',
+        'AuthLog',
+        'App',
+        'Admin'
+    ];
+
+    models.map(m => {
+        orm.loadCollection(Waterline.Collection.extend(require(`./${m}`)));
+        console.log(`Model ${m} loaded`)
+    });
 };
